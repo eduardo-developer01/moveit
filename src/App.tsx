0b04fbb97a8d5bc77aa8be00d561b4471fe6d062
Du <eduardosilvafreelancer@gmail.com>
2021-02-23 11:14:56 -0300

    component ExerienceBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import ExperienceBar from './components/ExperienceBar'
 import './styles/global.css'
 
 function App() {
   return (
-    <>
-      <h1 style={{ height: '3rem' }}>Hello World</h1>
-      <button style={{ height: '3rem' }}>button</button>
-    </>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   )
 }
 
---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,12 @@
+import react from 'react'
+
+export default function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp </span>
+      <div />
+      <span>60 xp</span>
+    </header>
+  )
+}
+
