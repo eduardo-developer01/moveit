ea08affd6cf3286ab78a3a212149f1b0eb72f8f0
Du <eduardosilvafreelancer@gmail.com>
2021-02-23 12:30:58 -0300

    class container

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -4,7 +4,10 @@ export default function ExperienceBar() {
   return (
     <header className="experience-bar">
       <span>0 xp </span>
-      <div />
+      <div>
+        <div style={{ width: '60%' }} />
+        <span className="current-experience" style={{ left: '60%' }}>250 xp</span>
+      </div>
       <span>60 xp</span>
     </header>
   )
